5b747d98c84a2a51889de86d541b6fcfbdf0b237
De-Proggremmer <ilya.mitrofanov07@gmail.com>
2025-09-30 22:10:07 +0300

    Homework_4

---
+++ Homework_4/ConsoleApplication.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+	float x, y;
+
+	cin >> x; cin >> y;
+
+	float R, S, C;
+
+	R = 1/cos(pow(y, 2)/x);
+	S = cbrt(x + log(1 / x));
+
+	C = max(R, S);
+
+	cout << "R = " << R << endl;
+	cout << "S = " << S << endl;
+	cout << "C = " << C << endl;
+
+	system("pause");
+}

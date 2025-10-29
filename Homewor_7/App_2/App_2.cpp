9a8553f860feaf42bc827cb9d5aa72226011ef71
De-Proggremmer <ilya.mitrofanov07@gmail.com>
2025-10-30 02:51:20 +0300

    Add files via upload

--- Homewor_7/App_2/App_2.cpp
+++ Homewor_7/App_2/App_2.cpp
@@ -1,18 +1,19 @@
-#include <iostream>
+﻿#include <iostream>
 #include <set>
+#include <algorithm>
 
 void diff_set(std::set<int>* A, std::set<int>* B);
 void printSet(std::set<int>* Set);
 
 int main()
 {
-	std::set<int> *A = new std::set<int>, *B = new std::set<int>;
+	std::set<int>* A = new std::set<int>, * B = new std::set<int>;
 
 	int N;
 	std::cout << "Enter lenth A: ";
 	std::cin >> N;
 	std::cout << "Enter set A elements: ";
-	for (int i=0; i < N; i++)
+	for (int i = 0; i < N; i++)
 	{
 		int a;
 		std::cin >> a;
@@ -41,19 +42,11 @@ void diff_set(std::set<int>* A, std::set<int>* B)
 {
 	std::set<int>* C = new std::set<int>;
 
-	for (auto i = B->begin(); i != B->end(); i++)
+	std::set_difference(B->begin(), B->end(), A->begin(), A->end(), std::inserter(*C, C->end()));
+
+	for (auto i = C->begin(); i != C->end(); i++)
 	{
-		bool flg = true;
-
-		for (auto j = A->begin(); j != A->end(); j++)
-		{
-			if (*i == *j || *i >= 0)
-			{
-				flg = false;
-			}
-		}
-
-		if (flg) { C->insert(*i); }
+		if (*i >= 0) { C->erase(i); }
 	}
 
 	printSet(C);

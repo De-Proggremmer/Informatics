cc88c7a29cc7dff58b2ae2bbd11ecc4848d0fb27
De-Proggremmer <ilya.mitrofanov07@gmail.com>
2026-02-26 01:02:52 +0300

    Add files via upload

---
+++ Alg_Lab_2/Alg_2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+    if (n % 2 == 1) n++; // Делаем n четным
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+
+    for (int i = 1; i < n; i += 2)
+        sum += 4 * f(a + i * h);
+
+    for (int i = 2; i < n - 1; i += 2)
+        sum += 2 * f(a + i * h);
+
+    return (h / 3) * sum;
+}
+
+double function_A(double x) {
+    return (x * x * x) - 12*(x * x) + 4; // Интегрируемая функция
+}
+
+double function_B(double x) {
+    return -1*(x * x * x) + (x * x) - 4; // Интегрируемая функция
+}
+
+int main() {
+    int N = 2;
+    double area_A = simpson_rule(function_A, 3, 6, N);
+    double area_B = simpson_rule(function_B, 3, 6, N);
+    std::cout << "Result Area: " << abs(area_A - area_B) << std::endl;
+    return 0;
+}
---
+++ Alg_Lab_2/Alg_2_test.cpp
@@ -0,0 +1,45 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+#include "../Alg_2/Alg_2.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Alg2test
+{
+	TEST_CLASS(Alg2test)
+	{
+	public:
+		
+		TEST_METHOD(AccuracyTest_2)
+		{
+			double result = abs(simpson_rule(function_A, 3, 6, 2) - simpson_rule(function_B, 3, 6, 2));
+			Assert::AreEqual(result, 187.50000, 0);
+		}
+		TEST_METHOD(AccuracyTest_4)
+		{
+			double result = abs(simpson_rule(function_A, 3, 6, 4) - simpson_rule(function_B, 3, 6, 4));
+			Assert::AreEqual(result, 187.50000, 1);
+		}
+		TEST_METHOD(AccuracyTest_8)
+		{
+			double result = abs(simpson_rule(function_A, 3, 6, 8) - simpson_rule(function_B, 3, 6, 8));
+			Assert::AreEqual(result, 187.50000, 2);
+		}
+		TEST_METHOD(AccuracyTest_16)
+		{
+			double result = abs(simpson_rule(function_A, 3, 6, 16) - simpson_rule(function_B, 3, 6, 16));
+			Assert::AreEqual(result, 187.50000, 3);
+		}
+		TEST_METHOD(AccuracyTest_32)
+		{
+			double result = abs(simpson_rule(function_A, 3, 6, 32) - simpson_rule(function_B, 3, 6, 32));
+			Assert::AreEqual(result, 187.5000, 4);
+		}
+		TEST_METHOD(AccuracyTest_64)
+		{
+			double result = abs(simpson_rule(function_A, 3, 6, 64) - simpson_rule(function_B, 3, 6, 64));
+			Assert::AreEqual(result, 187.5000, 5);
+		}
+	};
+}

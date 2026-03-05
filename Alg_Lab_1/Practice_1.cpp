dca09f294cf2c47ef0d9533eda9fd67ad42f5e80
De-Proggremmer <ilya.mitrofanov07@gmail.com>
2026-03-05 19:32:42 +0300

    Add files via upload

---
+++ Alg_Lab_1/Practice_1.cpp
@@ -0,0 +1,43 @@
+﻿
+
+#include <iostream>
+#include <map>
+#include <chrono>
+
+using namespace std;
+
+int add_to_dict(map<int, int> dict, int N)
+{
+    for (int i = 0; i < N; i++)
+    {
+        dict.insert({ i, 0 });
+    }
+    return N;
+}
+
+template <typename Func, typename... Args>
+auto measure_execution_time(Func&& func, Args&&... args) {
+    auto start_time = chrono::high_resolution_clock::now();  // Начинаем отсчет времени
+    auto result = func(forward<Args>(args)...);  // Выполняем переданную функцию с аргументами
+    auto end_time = chrono::high_resolution_clock::now();  // Останавливаем отсчет времени
+
+    auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);  // Вычисляем продолжительность в миллисекундах
+    return make_pair(result, duration.count());  // Возвращаем результат и время выполнения
+}
+
+
+int main()
+{
+    map<int, int> dict;
+
+    cout << measure_execution_time(add_to_dict, dict, 1000000).second << endl;
+    cout << measure_execution_time(add_to_dict, dict, 10000000).second << endl;
+    //cout << measure_execution_time(add_to_dict, dict, 4000000).second << endl;
+    //cout << measure_execution_time(add_to_dict, dict, 5000000).second << endl;
+    //cout << measure_execution_time(add_to_dict, dict, 6000000).second << endl;
+    //cout << measure_execution_time(add_to_dict, dict, 7000000).second << endl;
+    //cout << measure_execution_time(add_to_dict, dict, 8000000).second << endl;
+    //cout << measure_execution_time(add_to_dict, dict, 9000000).second << endl;
+
+    return 0;
+}
---
+++ Alg_Lab_1/UnitTest_p1.cpp
@@ -0,0 +1,52 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Practice_1/Practice_1.cpp"
+
+#include <map>
+#include <cmath>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestp1
+{
+	TEST_CLASS(TimeTest)
+	{
+	public:
+		
+		TEST_METHOD(TestOperationTime_1)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 10).second < std::pow(10, 2));
+		}
+		TEST_METHOD(TestOperationTime_2)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 100).second < std::pow(10, 4));
+		}
+		TEST_METHOD(TestOperationTime_3)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 1000).second < std::pow(10, 6));
+		}
+		TEST_METHOD(TestOperationTime_4)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 10000).second < std::pow(10, 8));
+		}
+		TEST_METHOD(TestOperationTime_5)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 100000).second < std::pow(10, 10));
+		}
+		TEST_METHOD(TestOperationTime_6)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 1000000).second < std::pow(10, 12));
+		}
+		TEST_METHOD(TestOperationTime_7)
+		{
+			std::map<int, int> dict;
+			Assert::IsTrue(measure_execution_time(add_to_dict, dict, 10000000).second < std::pow(10, 14));
+		}
+	};
+}
